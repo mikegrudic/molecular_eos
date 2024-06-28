c3b519955f2f75546f442f3d42c6f41feb07a217
Mike Grudic <mgrudic@pop-os.localdomain>
2024-06-28 15:20:20 -0700

    plot

--- plots.py
+++ plots.py
@@ -5,24 +5,25 @@ import numpy as np
 
 
 def do_plots():
-    Tgrid = np.logspace(1, 4, 10**5)
+    """Make plots of internal energy, heat capacity, and adiabatic index"""
+    Tgrid = np.logspace(1, 4.5, 10**6)
     e, cv, gamma = molecular_hydrogen_energy(Tgrid)
 
     fig, ax = plt.subplots(1, 3, figsize=(8, 3))
 
     kw = {"color": "black"}
-    ax[0].loglog(Tgrid, e / (BOLTZMANN * Tgrid), **kw)
-    ax[1].loglog(Tgrid, cv / BOLTZMANN, **kw)
-    ax[2].loglog(Tgrid, gamma, **kw)
+    ax[0].plot(Tgrid, e / (BOLTZMANN * Tgrid), **kw)
+    ax[1].plot(Tgrid, cv / BOLTZMANN, **kw)
+    ax[2].plot(Tgrid, gamma, **kw)
 
     for a in ax:
-        a.set(xlabel=r"$T\,\left(\rm K\right)$")
+        a.set(xlabel=r"$T\,\left(\rm K\right)$", xscale="log")
     ax[0].set_ylabel(r"$\langle E \rangle/k_{\rm B} T$")
     ax[1].set_ylabel(r"$c_{\rm V} / k_{\rm B}$")
     ax[2].set_ylabel(r"$\Gamma_{\rm 1}$")
     fig.tight_layout()
 
-    plt.savefig("H2_eos.pdf", bbox_inches="tight")
+    plt.savefig("H2_eos.png", bbox_inches="tight", dpi=300)
 
 
 if __name__ == "__main__":
